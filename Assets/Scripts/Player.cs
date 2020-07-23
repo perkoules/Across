b43a04d249d5c2cb58d313fc5bf22cc346be99be
PeriklisP <v8192218@live.tees.ac.uk>
2020-07-23 22:20:57 +0100

    Character Movement
    Testing cinemachine with character controller for third person movement.
    
    !- Check with camera as a child -!

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,19 +4,32 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    public CharacterController controller;
-    public float speed = 1.0f;
+    public CharacterController characterController;
+    public Transform mainCamera;
+    public float speed = 3f;
+    public float turnSmoothTime = 0.1f;
+    float turnSmoothVelocity;
 
-    // Update is called once per frame
+    private void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
     void Update()
     {
-
         float horizontal = Input.GetAxisRaw("Horizontal");
         float vertical = Input.GetAxisRaw("Vertical");
 
         Vector3 direction = new Vector3(horizontal, 0, vertical).normalized;
+        if(direction.magnitude >= 0.1f)
+        {
+            float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + mainCamera.eulerAngles.y;
+            float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, turnSmoothTime);
+            transform.rotation = Quaternion.Euler(0f, angle, 0f);
 
-        controller.Move(direction * speed * Time.deltaTime);
+            Vector3 moveDirection = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
+            characterController.Move(moveDirection.normalized * speed * Time.deltaTime);
+        }
 
     }
 }

a9fe5d3825e0b1286d4c05fa459ab4a57c8cbe57
PeriklisP <v8192218@live.tees.ac.uk>
2020-07-23 15:27:11 +0100

    Sample Scene - White Boxing

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public CharacterController controller;
+    public float speed = 1.0f;
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        float horizontal = Input.GetAxisRaw("Horizontal");
+        float vertical = Input.GetAxisRaw("Vertical");
+
+        Vector3 direction = new Vector3(horizontal, 0, vertical).normalized;
+
+        controller.Move(direction * speed * Time.deltaTime);
+
+    }
+}

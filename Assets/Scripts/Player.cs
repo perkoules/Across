913034e4c875b49fdc54075fd4d3d6d6cad9202b
PeriklisP <v8192218@live.tees.ac.uk>
2020-07-25 13:30:08 +0100

    Feature 01 Correction repo
    Corrected Player movement on mouse click
    Main Camera on the top/side

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -3,37 +3,30 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(Animator))]
 public class Player : MonoBehaviour
 {
-    private Rigidbody rb;
-    public float speed = 2.0f;
-    public GameObject cyl;
+    private Animator animator;
+    public float speed;
+    public Camera cam;
 
     private void Start()
     {
-        rb = GetComponent<Rigidbody>();
-        Cursor.lockState = CursorLockMode.Locked;
+        animator = GetComponent<Animator>();
     }
 
     void Update()
     {
-        float horizontal = Input.GetAxisRaw("Horizontal");
-        float vertical = Input.GetAxisRaw("Vertical");
-
-        Vector3 direction = new Vector3(horizontal, 0, vertical).normalized;
-
-        if (direction.magnitude >= 0.1f)
-        {
-            transform.position += direction * speed * Time.deltaTime;
-        }
+        
     }
 
     private void FixedUpdate()
     {
-        
-        Physics.Raycast(cyl.transform.position, Vector3.forward * 2, out RaycastHit hit, Mathf.Infinity);
-        Debug.DrawRay(cyl.transform.position, Vector3.forward * hit.distance, Color.green, 50);
-        Debug.Log(hit.collider);
+        Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+        if(Physics.Raycast(ray, out hit, 1000) && Input.GetKey(KeyCode.Mouse0) && hit.transform.gameObject.CompareTag("FloorCubeTag"))
+        {
+            transform.position = new Vector3(hit.transform.position.x, transform.position.y, hit.transform.position.z);
+        }
     }
-
 }

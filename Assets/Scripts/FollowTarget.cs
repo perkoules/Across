7f7772d947891a7f72d11d15740f5a0ad00822d9
PeriklisP <v8192218@live.tees.ac.uk>
2020-07-25 13:26:26 +0100

    Player Movement
    Implemented Player movement on mouse click

---
+++ Assets/Scripts/FollowTarget.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowTarget : MonoBehaviour
+{
+    
+    public GameObject targetToFollow;
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = new Vector3(targetToFollow.transform.position.x, transform.position.y, targetToFollow.transform.position.z);
+    }
+}
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -3,14 +3,15 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-[RequireComponent(typeof(Animator))]
 public class Player : MonoBehaviour
 {
-    private Animator animator;
+    private Rigidbody rb;
+    public float speed = 2.0f;
+    public GameObject cyl;
 
     private void Start()
     {
-        animator = GetComponent<Animator>();
+        rb = GetComponent<Rigidbody>();
         Cursor.lockState = CursorLockMode.Locked;
     }
 
@@ -18,7 +19,21 @@ public class Player : MonoBehaviour
     {
         float horizontal = Input.GetAxisRaw("Horizontal");
         float vertical = Input.GetAxisRaw("Vertical");
+
+        Vector3 direction = new Vector3(horizontal, 0, vertical).normalized;
+
+        if (direction.magnitude >= 0.1f)
+        {
+            transform.position += direction * speed * Time.deltaTime;
+        }
+    }
+
+    private void FixedUpdate()
+    {
         
+        Physics.Raycast(cyl.transform.position, Vector3.forward * 2, out RaycastHit hit, Mathf.Infinity);
+        Debug.DrawRay(cyl.transform.position, Vector3.forward * hit.distance, Color.green, 50);
+        Debug.Log(hit.collider);
     }
 
 }

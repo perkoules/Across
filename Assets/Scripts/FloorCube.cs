51cc4250e980b543a736d8c4b8bb4644fe2dcab1
PeriklisP <v8192218@live.tees.ac.uk>
2020-07-26 00:00:45 +0100

    Spawning and basic functionality

---
+++ Assets/Scripts/FloorCube.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FloorCube : MonoBehaviour
+{
+
+    public LayerMask layerMask;
+    public Shader originalShader;
+    public bool canBeStepped = false;
+
+
+
+}
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -3,30 +3,37 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-[RequireComponent(typeof(Animator))]
 public class Player : MonoBehaviour
 {
-    private Animator animator;
-    public float speed;
+    public CubeSpawner spawner;
     public Camera cam;
-
-    private void Start()
-    {
-        animator = GetComponent<Animator>();
-    }
-
-    void Update()
-    {
-        
-    }
-
+    public LayerMask layerMask;
+    public Collider[] colliders;
+    public Shader originalShader, greenShader;
+    
     private void FixedUpdate()
     {
-        Ray ray = cam.ScreenPointToRay(Input.mousePosition);
-        RaycastHit hit;
-        if(Physics.Raycast(ray, out hit, 1000) && Input.GetKey(KeyCode.Mouse0) && hit.transform.gameObject.CompareTag("FloorCubeTag"))
+        colliders = Physics.OverlapBox(transform.position, new Vector3(1.5f, 0.25f, 1.5f), Quaternion.identity, layerMask);        
+        foreach (var col in colliders)
         {
-            transform.position = new Vector3(hit.transform.position.x, transform.position.y, hit.transform.position.z);
+            col.GetComponent<MeshRenderer>().material.shader = greenShader;
+            col.gameObject.GetComponent<FloorCube>().canBeStepped = true;
         }
+        Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+        if (Physics.Raycast(ray, out RaycastHit hit, 1000) && Input.GetKey(KeyCode.Mouse0) && hit.transform.gameObject.CompareTag("FloorCubeTag"))
+        {
+            if (hit.collider.gameObject.GetComponent<FloorCube>().canBeStepped)
+            {
+                transform.position = new Vector3(hit.transform.position.x, transform.position.y, hit.transform.position.z);
+            }
+        }
+    }
+
+    void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+
+        //Draw a cube where the OverlapBox is (positioned where your GameObject is as well as a size)
+        Gizmos.DrawWireCube(transform.position, new Vector3(3, 0.5f, 3));
     }
 }

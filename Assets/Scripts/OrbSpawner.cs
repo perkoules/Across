ecc5b47e85622af7e1aff1e3c2b28d640ed3cad9
PeriklisP <v8192218@live.tees.ac.uk>
2020-07-26 19:44:10 +0100

    Orb Spawning
    Working materials, spawning, obtaining, showing on canvas UI.

--- Assets/Scripts/OrbSpawner.cs
+++ Assets/Scripts/OrbSpawner.cs
@@ -5,23 +5,27 @@ using UnityEngine;
 public class OrbSpawner : MonoBehaviour
 {
     public GameObject orb;
-    public Material orbMaterial;
+    public Material[] orbMaterials;
     private CubeSpawner cubeSpawner;
     void Start()
     {
-        StartCoroutine(SpawnOrbs());
-        
+        StartCoroutine(InitialOrbSpawn());        
     }
 
-    IEnumerator SpawnOrbs()
+    IEnumerator InitialOrbSpawn()
     {
         yield return new WaitForSeconds(5);
         cubeSpawner = FindObjectOfType<CubeSpawner>();
         cubeSpawner.floorTiles.RemoveAll(item => item == null);
-        for (int i = 0; i < 5; i++)
+        for (int i = 0; i < 10; i++)
         {
             int random = Random.Range(0, cubeSpawner.floorTiles.Count);
-            Instantiate(orb, cubeSpawner.floorTiles[random].transform.position + Vector3.down * 9.5f, Quaternion.identity);
+            GameObject orbClone = Instantiate(orb, cubeSpawner.floorTiles[random].transform.position + Vector3.down * 9.5f, Quaternion.identity);
+            orbClone.GetComponent<MeshRenderer>().material = orbMaterials[0];
+            if (i >= 5)
+            {
+                orbClone.GetComponent<MeshRenderer>().material = orbMaterials[1];
+            }
         }
     }
 }
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -2,6 +2,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Player : MonoBehaviour
 {
@@ -9,7 +10,8 @@ public class Player : MonoBehaviour
     public LayerMask layerMask;
     public Collider[] colliders;
     public Shader originalShader, greenShader;
-
+    public Text[] orbText;
+    public int[] orbsObtained = new int[7] { 0, 0, 0, 0, 0, 0, 0 };
     private void FixedUpdate()
     {
         colliders = Physics.OverlapBox(transform.position, new Vector3(1.5f, 0.25f, 1.5f), Quaternion.identity, layerMask);        
@@ -31,8 +33,48 @@ public class Player : MonoBehaviour
     {
         if (other.gameObject.CompareTag("OrbTag"))
         {
-            Destroy(other.gameObject);
-            print("Orb Obtained");
+            if (other.gameObject.GetComponent<MeshRenderer>().material.name.Contains("Red"))
+            {
+                orbsObtained[0]++;
+                orbText[0].text = orbsObtained[0].ToString();
+                Destroy(other.gameObject);
+            }
+            else if (other.gameObject.GetComponent<MeshRenderer>().material.name.Contains("Orange"))
+            {
+                orbsObtained[1]++;
+                orbText[1].text = orbsObtained[1].ToString();
+                Destroy(other.gameObject);
+            }
+            else if (other.gameObject.GetComponent<MeshRenderer>().material.name.Contains("Yellow"))
+            {
+                orbsObtained[2]++;
+                orbText[2].text = orbsObtained[2].ToString();
+                Destroy(other.gameObject);
+            }
+            else if (other.gameObject.GetComponent<MeshRenderer>().material.name.Contains("Green"))
+            {
+                orbsObtained[3]++;
+                orbText[3].text = orbsObtained[3].ToString();
+                Destroy(other.gameObject);
+            }
+            else if (other.gameObject.GetComponent<MeshRenderer>().material.name.Contains("Blue"))
+            {
+                orbsObtained[4]++;
+                orbText[4].text = orbsObtained[4].ToString();
+                Destroy(other.gameObject);
+            }
+            else if (other.gameObject.GetComponent<MeshRenderer>().material.name.Contains("Indigo"))
+            {
+                orbsObtained[5]++;
+                orbText[5].text = orbsObtained[5].ToString();
+                Destroy(other.gameObject);
+            }
+            else if (other.gameObject.GetComponent<MeshRenderer>().material.name.Contains("Violet"))
+            {
+                orbsObtained[6]++;
+                orbText[6].text = orbsObtained[6].ToString();
+                Destroy(other.gameObject);
+            }
         }
     }
 }
